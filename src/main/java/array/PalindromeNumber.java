a1b09f9bbe39a767e3c9ea6cdf3d2d07990dee3b
Vysakh-vn <vysakhvn28@gmail.com>
2024-04-21 19:02:14 +0530

    Palindrome Number

---
+++ src/main/java/array/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package array;
+
+/**
+ * @author Vysakh VN
+ * @since 2024-04-21
+ */
+public class PalindromeNumber {
+
+	
+	/**
+	 * Checks if a given integer is a palindrome.
+	 * 
+	 * @param x The integer to check for palindromicity.
+	 * @return True if the number is a palindrome, false otherwise.
+	 */
+	public boolean isPalindrome(int x) {
+
+		int duplicate = x;
+		int reverse = 0;
+		while (x > 0) {
+			int reminder = x % 10;
+			x = x / 10;
+			reverse = reverse * 10 + reminder;
+		}
+		if (reverse == duplicate) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
