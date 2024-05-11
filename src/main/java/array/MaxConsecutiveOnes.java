669129a9d7549a58f616ba1ea45894aca79c5b63
Vysakh-vn <vysakhvn28@gmail.com>
2024-05-11 19:38:23 +0530

    Added comments

--- src/main/java/array/MaxConsecutiveOnes.java
+++ src/main/java/array/MaxConsecutiveOnes.java
@@ -1,7 +1,17 @@
 package array;
 
+/**
+ * @author Vysakh VN
+ * @since 2024-05-11
+ */
 public class MaxConsecutiveOnes {
 
+	/**
+	 * Finds the maximum number of consecutive ones in a given integer array.
+	 * 
+	 * @param nums An integer array.
+	 * @return The maximum number of consecutive ones in the array.
+	 */
 	public int findMaxConsecutiveOnes(int[] nums) {
 
 		int maxOnes = 0;
