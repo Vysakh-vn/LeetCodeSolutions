39bb8f7e7d6dadb6fa5c8e075d2a448bc3da96a2
Vysakh-vn <vysakhvn28@gmail.com>
2024-08-22 13:11:52 +0530

    Added  junit dependencies

---
+++ src/test/java/string/LengthOfLastWordTest.java
@@ -0,0 +1,14 @@
+package string;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LengthOfLastWordTest {
+	
+	@Test
+	public void addTest () {
+		assertEquals(4, 2 + 2);
+	}
+
+}
