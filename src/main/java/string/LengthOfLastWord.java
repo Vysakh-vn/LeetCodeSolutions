2d00786aaf0c301e56a3dda65f400f4a76c0381a
Vysakh-vn <vysakhvn28@gmail.com>
2024-08-22 15:25:40 +0530

    Solution of Length of last word

--- src/main/java/string/LengthOfLastWord.java
+++ src/main/java/string/LengthOfLastWord.java
@@ -1,5 +1,28 @@
 package string;
 
+/**
+ * @author Vysakh VN
+ *
+ */
 public class LengthOfLastWord {
+	
+	
+	/**
+	 * Function to find the length of last word in the sentence.
+	 * 
+	 * @param s sentence from which to find the last word's length.
+	 * @return the of length word in the sentence
+	 */
+	public int getLengthOfLastWord(String s) {		
+		String m = s.trim();
+		int i = m.length();
+		int l = 0;
+		while(i > 0 && m.charAt(i - 1) != ' ') {
+			l ++;
+			i --;
+		}
+		return l;
+	}
+	
 
 }
