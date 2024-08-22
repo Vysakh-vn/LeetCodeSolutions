f9ac914967d43ba5accb05d95401988b5b513e3b
Vysakh-vn <vysakhvn28@gmail.com>
2024-08-22 15:26:23 +0530

    Test case for Length of last word

--- src/test/java/string/LengthOfLastWordTest.java
+++ src/test/java/string/LengthOfLastWordTest.java
@@ -3,12 +3,20 @@ package string;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LengthOfLastWordTest {
 	
-	@Test
-	public void addTest () {
-		assertEquals(4, 2 + 2);
+	@ParameterizedTest
+	@CsvSource({
+		"Hello word, 4",
+		"Hello word , 4",
+		"'', 0"
+	})
+	public void lengthOfLastWordTest(String sentence, int expectedLength) {
+		int actualLength = new LengthOfLastWord().getLengthOfLastWord(sentence);
+		assertEquals(expectedLength, actualLength);
 	}
 
 }
