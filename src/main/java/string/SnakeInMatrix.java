79c4bc5a9eeed0aab1018329d015f10035d63797
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-01 17:54:06 +0530

    Added comments for snake in matrix solution

--- src/main/java/string/SnakeInMatrix.java
+++ src/main/java/string/SnakeInMatrix.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class SnakeInMatrix {
 	
+    /**
+     * Finds the position of snake in a matrix after a series of commands.
+     * 
+     * @param n Size of the matrix
+     * @param commands
+     * @return final position of snake.
+     */
     public int finalPositionOfSnake(int n, List<String> commands) { 
         int i=0, j=0;
         for(String command : commands){
