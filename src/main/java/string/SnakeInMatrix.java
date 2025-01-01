10794cf9bb61ae76a5ecdf19b194c7369234458d
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-01 17:48:09 +0530

    Solution of Snake in matrix.

---
+++ src/main/java/string/SnakeInMatrix.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.List;
+
+public class SnakeInMatrix {
+	
+    public int finalPositionOfSnake(int n, List<String> commands) { 
+        int i=0, j=0;
+        for(String command : commands){
+            if(command.equals("RIGHT")){
+              j=j+1;
+            }else if(command.equals("LEFT")){
+               j=j-1;
+            }else if(command.equals("DOWN")){
+               i=i+1;
+            }else {
+               i=i-1;
+            }
+        }
+        return (i*n)+j;
+    }
+}
