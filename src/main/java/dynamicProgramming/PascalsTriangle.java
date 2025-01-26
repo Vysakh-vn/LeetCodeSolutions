8087da46c444057d9766dacbe15ec4b6aab3b8ef
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-26 19:31:16 +0530

    Solved pascals triangle

---
+++ src/main/java/dynamicProgramming/PascalsTriangle.java
@@ -0,0 +1,45 @@
+package dynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PascalsTriangle {
+	
+	Map<Integer, List<Integer>> memory = new HashMap<>();
+	
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> list = new ArrayList<>();
+		generateRow(numRows);
+		for(int i = 1; i <= numRows; i ++) {
+			list.add(memory.get(i));
+		}
+		return list;
+	}
+	
+	public List<Integer> generateRow(int numRows) {
+		if(numRows == 1) {
+			List<Integer> list = new ArrayList<>();
+			list.add(1);
+			memory.put(1, list);
+			return list;
+		}
+		List<Integer> previousRow = generateRow(numRows - 1);
+		List<Integer> row = new ArrayList<>();
+		for(int i = 0; i < numRows; i ++) {
+			if(i == 0) {
+				row.add(1);
+			} else if (i == numRows - 1) {
+				row.add(1);
+			} else {
+				int value = previousRow.get(i - 1) + previousRow.get(i);
+				row.add(value);
+			}
+			
+		}
+		memory.put(numRows, row);
+		return row;
+	}
+
+}
