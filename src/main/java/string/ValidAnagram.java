552d20622a880b1521ecbf211720fd99ed5bf590
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-15 18:37:07 +0530

    Valid anagram solution added

---
+++ src/main/java/string/ValidAnagram.java
@@ -0,0 +1,38 @@
+package string;
+
+public class ValidAnagram {
+	
+    public boolean isAnagram(String s, String t) {
+        String sortedSource = quickSort(s);
+        String sortedTarget = quickSort(t);
+        return sortedSource.equals(sortedTarget);
+    }
+
+    public String quickSort(String str) {
+        if(str.length() <= 1) {
+            return str;
+        }
+
+        char pivot = str.charAt(0);
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+
+        for(int i = 1; i < str.length(); i ++) {
+            char c = str.charAt(i);
+            if(c <= pivot) {
+                left.append(c);
+            } else {
+                right.append(c);
+            }
+        }
+
+        String leftStr = quickSort(left.toString());
+        String rightStr = quickSort(right.toString());
+
+        StringBuilder sorted = new StringBuilder();
+        sorted.append(leftStr).append(pivot).append(rightStr);
+
+        return sorted.toString(); 
+    }
+
+}
