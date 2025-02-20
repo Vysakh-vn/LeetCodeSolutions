f86728324a1152e2031489ae93c7f9650bcea517
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-20 20:39:26 +0530

    Solution of middle node of linked list

--- src/main/java/linkedlist/MiddleNode.java
+++ src/main/java/linkedlist/MiddleNode.java
@@ -1,5 +1,28 @@
 package linkedlist;
 
 public class MiddleNode {
+	
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null) {
+            if(fast.next.next == null) {
+                fast = fast.next;
+            } else {
+                fast = fast.next.next;
+            }
+            slow = slow.next;
+        }
+        return slow;
+    }
 
 }
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
