fd8e7dd9c05451743c87e05c4e7ba2e5ec0df85d
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-23 12:22:20 +0530

    Added comments for search insert position solution

--- src/main/java/array/SearchInsertPosition.java
+++ src/main/java/array/SearchInsertPosition.java
@@ -2,6 +2,14 @@ package array;
 
 public class SearchInsertPosition {
 	
+    /**
+     * Function to find the insert position of an integer,
+     * return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
     public int searchInsert(int[] nums, int target) {
         return binarySearch(nums, target, 0, nums.length - 1);
     }
