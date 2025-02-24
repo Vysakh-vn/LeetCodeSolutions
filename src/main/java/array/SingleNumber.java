74c8fe5f62f3dcfd46fd269eb5d34ff3e82d91f4
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-24 19:20:59 +0530

    Added comments for single number

--- src/main/java/array/SingleNumber.java
+++ src/main/java/array/SingleNumber.java
@@ -2,6 +2,12 @@ package array;
 
 public class SingleNumber {
 	
+    /**
+     * Function to find the unique number in a list of numbers
+     * 
+     * @param nums
+     * @return
+     */
     public int singleNumber(int[] nums) {
         int unique = 0;
         for(int n : nums) {
