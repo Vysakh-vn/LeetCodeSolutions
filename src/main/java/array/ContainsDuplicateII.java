f170f8127e1b1cae3572c7299c9ec2c1e88d5920
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-27 20:24:37 +0530

    Added comments for contains duplicate II

--- src/main/java/array/ContainsDuplicateII.java
+++ src/main/java/array/ContainsDuplicateII.java
@@ -5,6 +5,14 @@ import java.util.Map;
 
 public class ContainsDuplicateII {
 	
+	/**
+	 * Checks if the given array contains duplicate elements within a given index distance.
+	 *
+	 * @param nums the input array of integers
+	 * @param k the maximum allowed index difference between duplicate values
+	 * @return true if there are two distinct indices i and j such that nums[i] == nums[j] 
+	 *         and the absolute difference between i and j is at most k, otherwise false
+	 */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
         Map<Integer, Integer> map = new HashMap<>();
