61245c6b1c913d23b423266060b0571881849fa7
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-27 19:54:43 +0530

    Added comments for MissingNumber

--- src/main/java/array/MissingNumber.java
+++ src/main/java/array/MissingNumber.java
@@ -2,6 +2,12 @@ package array;
 
 public class MissingNumber {
 	
+	/**
+	 * Finds the missing number in an array containing n distinct numbers in the range [0, n].
+	 * 
+	 * @param nums the input array of distinct numbers
+	 * @return the missing number in the sequence
+	 */
 	public int missingNumber(int[] nums) {
         int reqSum = 0;
         int actSum = 0;
