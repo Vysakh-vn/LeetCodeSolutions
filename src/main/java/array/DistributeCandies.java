ec4af74a127751c1c3d306541620be6d44c0860d
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-01 09:56:50 +0530

    Added comments for distribute candies

--- src/main/java/array/DistributeCandies.java
+++ src/main/java/array/DistributeCandies.java
@@ -5,6 +5,17 @@ import java.util.Set;
 
 public class DistributeCandies {
 	
+	/**
+	 * Determines the maximum number of unique candy types Alice can eat.
+	 *
+	 * <p>Given an integer array representing different types of candies, 
+	 * this function calculates the maximum number of unique candies that 
+	 * can be distributed to Alice. The total number of candies is 
+	 * always even, and she can eat at most half of them.</p>
+	 *
+	 * @param candyType An array of integers representing different candy types.
+	 * @return The maximum number of unique candy types Alice can eat.
+	 */
     public int distributeCandies(int[] candyType) {
         int l = candyType.length;
         Set<Integer> set = new HashSet<>();
