cc26867521ab9efc89f66c603c71952e65628449
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-02 16:09:21 +0530

    Added comment for Intersection of Two array

--- src/main/java/array/IntersectionOfTwoArray.java
+++ src/main/java/array/IntersectionOfTwoArray.java
@@ -6,6 +6,17 @@ import java.util.Set;
 
 public class IntersectionOfTwoArray {
 
+	/**
+	 * Finds the intersection of two integer arrays.
+	 *
+	 * <p>This method takes two integer arrays and returns an array containing 
+	 * the unique elements that are common in both input arrays.</p>
+	 *
+	 *
+	 * @param nums1 The first input array of integers.
+	 * @param nums2 The second input array of integers.
+	 * @return An array containing the unique common elements of both arrays.
+	 */
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
