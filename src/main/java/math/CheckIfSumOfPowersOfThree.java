d376fe7899683a08a2eae92416a03d0c3bbcfe32
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-04 21:59:35 +0530

    Solution of Check if number is sum of power or three

---
+++ src/main/java/math/CheckIfSumOfPowersOfThree.java
@@ -0,0 +1,13 @@
+package math;
+
+public class CheckIfSumOfPowersOfThree {
+	
+    public boolean checkPowersOfThree(int n) {
+        while(n >= 3){
+            if(n % 3 == 2) return false;
+            n /= 3;
+        }
+        return n!= 2;
+    }
+
+}
