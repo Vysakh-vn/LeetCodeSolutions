add54448be1856cd40cc2dd1e19915ab948a2e6b
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-05 21:36:22 +0530

    Solution of Count Total Number Of Coloured Cells

--- src/main/java/recursion/CountTotalNumberOfColouredCells.java
+++ src/main/java/recursion/CountTotalNumberOfColouredCells.java
@@ -1,5 +1,12 @@
 package recursion;
 
 public class CountTotalNumberOfColouredCells {
+	
+    public long coloredCells(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        return 4 * (n - 1) + coloredCells(n - 1);
+    }
 
 }
