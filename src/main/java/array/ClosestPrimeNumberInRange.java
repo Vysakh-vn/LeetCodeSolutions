84ff57aa28597337b8bc5f822021b2e0f0f02376
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-07 21:44:30 +0530

    Solution of Closest Prime Number In Range

---
+++ src/main/java/array/ClosestPrimeNumberInRange.java
@@ -0,0 +1,53 @@
+package array;
+
+import java.util.Arrays;
+
+public class ClosestPrimeNumberInRange {
+
+	
+    public int[] closestPrimes(int left, int right) {
+        int[] temp = new int[right - left];
+        int count = 0;
+        for(int i = left; i <= right; i ++) {
+            if(isPrime(i)) {
+                temp[count] = i;
+                count ++;
+            }
+        }
+        int[] primes = Arrays.copyOf(temp, count);
+        int[] result = new int[2];
+        if(primes.length < 2) {
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+
+        int leastDiff = primes[1] - primes[0];
+        result[0] = primes[0];
+        result[1] = primes[1];
+        for(int j = 1; j < primes.length - 1; j ++) {
+            int diff = primes[j + 1] - primes[j];
+            if(diff < leastDiff) {
+                result[0] = primes[j];
+                result[1] = primes[j + 1];
+                leastDiff = diff;
+            }
+        }
+        return result;
+    }
+
+    private boolean isPrime(int n) {
+
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0) return false;
+
+        for(int i = 3; i * i <= n; i += 2) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+	
+}
