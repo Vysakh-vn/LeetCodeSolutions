e764adf2540998e2183420534d0b1d0bae2c28b8
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-08 22:04:45 +0530

    Added comments

--- src/main/java/array/MergeSortedArray.java
+++ src/main/java/array/MergeSortedArray.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 	
+	
+    /**
+     * Function to merge to sorted array
+     * 
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for(int i = 0; i < n; i ++ ) {
             nums1[m + i] = nums2[i];
