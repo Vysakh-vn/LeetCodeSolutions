ee23f858c5992f5c2721528907ed88c620928ad5
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-09 20:38:22 +0530

    Added comments

--- src/main/java/string/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
+++ src/main/java/string/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
@@ -2,6 +2,21 @@ package string;
 
 public class MinimumRecolorsToGetKConsecutiveBlackBlocks {
 	
+	/**
+	 * This method finds the minimum number of recolors needed to get at least `k` consecutive 'B' (black) blocks
+	 * in the given string `blocks`. It uses a sliding window approach to efficiently determine the minimum number 
+	 * of 'W' (white) blocks that need to be changed to 'B' within any `k`-sized contiguous segment.
+	 * 
+	 * @param blocks A string consisting of characters 'B' and 'W', representing a sequence of blocks.
+	 * @param k The required length of consecutive 'B' blocks.
+	 * @return The minimum number of 'W' blocks that must be recolored to form a contiguous sequence of `k` 'B' blocks.
+	 * 
+	 * Approach:
+	 * - First, count the number of 'W' blocks in the initial window of size `k`.
+	 * - Use a sliding window technique to move through `blocks`, updating the count of 'W' blocks efficiently.
+	 * - Track the minimum number of 'W' blocks found in any window, as that will be the answer.
+	 * - Return the minimum count of 'W' that need to be recolored.
+	 */
     public int minimumRecolors(String blocks, int k) {
         int w = 0;
         int least = 0;
