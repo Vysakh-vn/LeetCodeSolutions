fefed92bacd53d7752b71e12e0996d2eb7c7670d
Vysakh-vn <vysakhvn28@gmail.com>
2025-03-20 20:07:40 +0530

    Added solution of valid parantheses

--- src/main/java/stack/ValidParantheses.java
+++ src/main/java/stack/ValidParantheses.java
@@ -1,5 +1,24 @@
 package stack;
 
+import java.util.Stack;
+
 public class ValidParantheses {
+	
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if( c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+            	if(stack.isEmpty()) return false;
+                    char r = stack.pop();
+                    if(r == '(' && c != ')') return false;
+                    if(r == '{' && c != '}') return false;
+                    if(r == '[' && c != ']') return false;
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
 
 }
