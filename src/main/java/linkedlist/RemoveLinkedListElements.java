03432bf4a202fd7c1ccf1d3af66c306b227ec111
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-06 19:16:11 +0530

    Added comments

--- src/main/java/linkedlist/RemoveLinkedListElements.java
+++ src/main/java/linkedlist/RemoveLinkedListElements.java
@@ -2,6 +2,18 @@ package linkedlist;
 
 public class RemoveLinkedListElements {
 
+	/**
+	 * Removes all nodes from a singly linked list that have a specific value.
+	 *
+	 * This method traverses the given linked list and removes all nodes whose value
+	 * equals the specified target value. The method handles cases where the nodes
+	 * to be removed include the head of the list. It returns the new head of the
+	 * modified list after removals.
+	 *
+	 * @param head The head of the singly linked list.
+	 * @param val The value to be removed from the list.
+	 * @return The head of the list after removing all nodes with the given value.
+	 */
     public ListNode removeElements(ListNode head, int val) {
         ListNode current = head;
         ListNode previous = null;
