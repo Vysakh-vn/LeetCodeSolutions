ff09bf772a09ada4ee8b1a64a97cd258bf177c2a
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-10 16:00:46 +0530

    Added comments for the solution

--- src/main/java/array/NextGreaterElementI.java
+++ src/main/java/array/NextGreaterElementI.java
@@ -5,6 +5,17 @@ import java.util.Map;
 
 public class NextGreaterElementI {
 	
+	/**
+	 * Finds the next greater element for each element in nums1 based on their positions in nums2.
+	 *
+	 * For each element in nums1, this method finds the index of that element in nums2,
+	 * then searches to the right of that index in nums2 for the first element that is greater.
+	 * If no such element is found, -1 is returned for that position.
+	 *
+	 * @param nums1 An array of integers representing the subset of elements to find next greater values for.
+	 * @param nums2 An array of integers representing the superset in which next greater elements are searched.
+	 * @return An array where each element corresponds to the next greater value of nums1[i] in nums2, or -1 if none exists.
+	 */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> lookup = new HashMap<>();
 		int[] result = new int[nums1.length];
