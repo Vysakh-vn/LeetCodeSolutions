f91a629ab28fbee53381c4ab417fbb5d6c287703
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-10 17:54:44 +0530

    Added comment Next Greater Element II solution

--- src/main/java/array/NextGreaterElementII.java
+++ src/main/java/array/NextGreaterElementII.java
@@ -2,6 +2,17 @@ package array;
 
 public class NextGreaterElementII {
 	
+	/**
+	 * Finds the next greater element for each element in a circular array.
+	 *
+	 * For each element in the input array, this method searches forward (with wrapping around to the beginning of the array)
+	 * to find the next element that is greater than the current one. If no such element exists, -1 is returned for that position.
+	 *
+	 * This simulates a circular array where the end connects to the beginning.
+	 *
+	 * @param nums An array of integers representing the circular input.
+	 * @return An array where each element is the next greater value in the circular array, or -1 if none exists.
+	 */
 	public int[] nextGreaterElements(int[] nums) {
 		int[] result = new int[nums.length];
 		for (int i = 0; i < nums.length; i++) {
