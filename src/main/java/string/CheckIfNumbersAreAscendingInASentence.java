5c212770693b128ab988876ec964e4fd09ee56a2
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-11 11:14:29 +0530

    Added comments for the solution

--- src/main/java/string/CheckIfNumbersAreAscendingInASentence.java
+++ src/main/java/string/CheckIfNumbersAreAscendingInASentence.java
@@ -8,6 +8,15 @@ public class CheckIfNumbersAreAscendingInASentence {
 	
 	private Set<Character> set = new HashSet<>(Arrays.asList('0','1','2','3','4','5','6','7','8','9'));
 	
+	/**
+	 * Determines whether all numbers in a sentence are in strictly increasing order.
+	 *
+	 * The input string contains words and numbers separated by spaces. This method extracts all numeric values
+	 * and checks whether they are in strictly increasing order as they appear in the string.
+	 *
+	 * @param s The input sentence containing words and numbers separated by spaces.
+	 * @return true if the numbers appear in strictly increasing order, false otherwise.
+	 */
     public boolean areNumbersAscending(String s) {
     	String[] words = s.split(" ");
     	int previous = 0;
