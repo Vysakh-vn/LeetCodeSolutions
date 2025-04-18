b4a6a752f28d1811ea5ffb3518c44c26e4cc3e53
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-18 15:33:34 +0530

    Added comments for the solution

--- src/main/java/linkedlist/DeleteNodeInALinkedList.java
+++ src/main/java/linkedlist/DeleteNodeInALinkedList.java
@@ -2,6 +2,11 @@ package linkedlist;
 
 public class DeleteNodeInALinkedList {
 	
+    /**
+     * Deletes a node from the linked list without having head pointer.
+     * 
+     * @param node
+     */
     public void deleteNode(ListNode node) {
         int val = node.val;
         ListNode prev = null;
