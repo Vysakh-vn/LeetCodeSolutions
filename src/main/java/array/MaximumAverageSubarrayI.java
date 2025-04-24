3d5e462328d502df55a26d6213d7c8e95ec5545c
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-24 14:48:34 +0530

    Solution of Maximum average subarray I

---
+++ src/main/java/array/MaximumAverageSubarrayI.java
@@ -0,0 +1,23 @@
+package array;
+
+public class MaximumAverageSubarrayI {
+	
+    public double findMaxAverage(int[] nums, int k) {
+    	double maxAverage = 0;
+    	double sum = 0;
+    	for(int i = 0; i < k; i ++) {
+    		sum = sum + nums[i];
+    	}
+    	maxAverage = sum / k;
+    	
+    	for(int i = k; i < nums.length; i ++) {
+    		sum = (sum - nums[i - k]) + nums[i];
+    		double avg = sum / k;
+    		if(avg > maxAverage) {
+    			maxAverage = avg;
+    		}
+    	}
+        return maxAverage;
+    }
+
+}
