3b314cf5b094b421f54815675336375b110b7c43
Vysakh-vn <vysakhvn28@gmail.com>
2025-04-28 20:24:36 +0530

    Added comments for the solution

--- src/main/java/slidingwindow/LongestEvenOddSubarrayWithThreshold.java
+++ src/main/java/slidingwindow/LongestEvenOddSubarrayWithThreshold.java
@@ -2,6 +2,17 @@ package slidingwindow;
 
 public class LongestEvenOddSubarrayWithThreshold {
 
+	/**
+	 * Length of the longest subarray of nums starting at index l and ending at index r (0 <= l <= r < nums.length) that satisfies the following conditions:
+	 * 
+	 * nums[l] % 2 == 0
+	 * For all indices i in the range [l, r - 1], nums[i] % 2 != nums[i + 1] % 2
+	 * For all indices i in the range [l, r], nums[i] <= threshold
+	 * 
+	 * @param nums
+	 * @param threshold
+	 * @return
+	 */
 	public int longestAlternatingSubarray(int[] nums, int threshold) {
 		int l = 0;
 		int r = 0;
