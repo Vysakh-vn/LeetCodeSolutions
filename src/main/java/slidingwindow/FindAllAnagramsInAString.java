698651c39b112f76612eef5976546a1f95105328
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-01 18:27:56 +0530

    Solution of Find all anagrams in a string

---
+++ src/main/java/slidingwindow/FindAllAnagramsInAString.java
@@ -0,0 +1,36 @@
+package slidingwindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagramsInAString {
+	
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] lookup = new int[26];
+        List<Integer> list = new ArrayList<>();
+        for(char c : p.toCharArray()) {
+            lookup[c - 'a'] ++;
+        }
+        boolean anagramFound = false;
+        for(int left = 0; left <= (s.length() - p.length()); left ++) {
+            char c = s.charAt(left);
+            if(lookup[c - 'a'] > 0) {
+                int[] copy = Arrays.copyOf(lookup, 26);
+                copy[c - 'a'] --;
+                anagramFound = true;
+                for(int right = 1; right < p.length();  right ++) {
+                    char next = s.charAt(left + right);
+                    if(copy[next - 'a'] > 0) copy[next - 'a'] --;
+                    else {
+                        anagramFound = false;
+                        break;
+                    }
+                }
+                if(anagramFound == true) list.add(left);
+            }
+        }
+        return list;
+    }
+
+}
