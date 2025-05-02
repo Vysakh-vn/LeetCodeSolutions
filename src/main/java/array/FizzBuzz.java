902bc83f7be6d63a4c014b372ae16e37503e194f
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-02 13:41:15 +0530

    Added comment for the solution

--- src/main/java/array/FizzBuzz.java
+++ src/main/java/array/FizzBuzz.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class FizzBuzz {
 	
+    /**
+     * Function returns a string array answer (1-indexed) where:
+     * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+     * answer[i] == "Fizz" if i is divisible by 3.
+     * answer[i] == "Buzz" if i is divisible by 5.
+     * answer[i] == i (as a string) if none of the above conditions are true.
+     * 
+     * @param n
+     * @return
+     */
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>(); 
         for(int i = 1; i <= n; i ++) {
