36c47e135471071cf1f61fcce8e64afad9ca7eb3
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-02 11:05:22 +0530

    Solution of Binary Tree In order traversal

--- src/main/java/tree/BinaryTreeInOrderTraversal.java
+++ src/main/java/tree/BinaryTreeInOrderTraversal.java
@@ -1,5 +1,43 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTreeInOrderTraversal {
 
+	List<Integer> list = new ArrayList<>();
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+		inOrderTraversal(root);
+		return list;
+	}
+
+	public void inOrderTraversal(TreeNode node) {
+		if (node == null)
+			return;
+		inOrderTraversal(node.left);
+		list.add(node.val);
+		inOrderTraversal(node.right);
+	}
+
+	class TreeNode {
+
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+	}
 }
