f353072872d3984ff24c1eb54ed81b36c74a0e69
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-03 12:45:16 +0530

    Added solution of Binary Tree post order traversal

---
+++ src/main/java/tree/BinaryTreePostOrderTraversal.java
@@ -0,0 +1,44 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePostOrderTraversal {
+	
+    List<Integer> list = new LinkedList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        doPostOrderTraversal(root);
+        return list;
+    }
+
+    public void doPostOrderTraversal(TreeNode root) {
+        if(root == null) return;
+        
+        doPostOrderTraversal(root.left);
+        doPostOrderTraversal(root.right);
+        list.add(root.val);
+    }
+    
+	class TreeNode {
+
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+	}
+
+}
