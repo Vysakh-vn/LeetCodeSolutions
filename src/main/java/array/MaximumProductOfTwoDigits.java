f1562d7ee11f538695273f57b5c2b57a4db46086
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-04 13:17:05 +0530

    Added solution of Maximum Product of two digits

---
+++ src/main/java/array/MaximumProductOfTwoDigits.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Arrays;
+
+public class MaximumProductOfTwoDigits {
+	
+    public int maxProduct(int n) {
+        
+        int divisor = 10;
+        int length = 2;
+        while(divisor < n) {
+            if((divisor * 10) < n) {
+                divisor = divisor * 10;
+                length ++;
+            } else {
+                break;
+            }
+        }
+        
+        int[] digits = new int[length];
+        int count = 0;
+        while(count < length) {
+            int digit = n / divisor;
+            digits[count] = digit;
+            count ++;
+            n = n % divisor;
+            divisor = divisor / 10;
+        }
+        
+        Arrays.sort(digits);
+        return digits[length - 1] * digits[length - 2];
+        
+    }
+
+}
