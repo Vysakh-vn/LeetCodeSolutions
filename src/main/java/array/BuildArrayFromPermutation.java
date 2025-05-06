c2e74810887d8660abbae389f8b79ccc5feaa465
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-06 18:15:33 +0530

    Added comment for the solution

--- src/main/java/array/BuildArrayFromPermutation.java
+++ src/main/java/array/BuildArrayFromPermutation.java
@@ -2,6 +2,12 @@ package array;
 
 public class BuildArrayFromPermutation {
 	
+    /**
+     * Build an array ans of the same length of given array nums where ans[i] = nums[nums[i]] for each 0 <= i < nums.length.
+     * 
+     * @param nums
+     * @return
+     */
     public int[] buildArray(int[] nums) {
         int[] ret = new int[nums.length];
         for(int i = 0; i < nums.length; i ++) {
