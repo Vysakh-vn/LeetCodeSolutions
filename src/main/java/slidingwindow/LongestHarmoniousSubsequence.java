ab7a2c15d3a17b62be98a18b1d570913cdc4af80
Vysakh-vn <vysakhvn28@gmail.com>
2025-05-07 19:44:25 +0530

    Added comment for the solution

--- src/main/java/slidingwindow/LongestHarmoniousSubsequence.java
+++ src/main/java/slidingwindow/LongestHarmoniousSubsequence.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 
 public class LongestHarmoniousSubsequence {
 	
+	/**
+	 * Finds the length of the longest harmonious subsequence in the given array.
+	 * A harmonious subsequence is where the difference between the maximum and 
+	 * minimum values is exactly 1.
+	 *
+	 * @param nums The input array of integers.
+	 * @return The length of the longest harmonious subsequence.
+	 */
     public int findLHS(int[] nums) {
         Arrays.sort(nums);
         int maxCount = 0;
