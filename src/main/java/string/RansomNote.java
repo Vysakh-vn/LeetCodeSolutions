4b438f08fcba5f911c57de014a984f91da09e3b8
Vysakh-vn <vysakhvn28@gmail.com>
2025-09-04 13:57:43 +0530

    Optimised solution

--- src/main/java/string/RansomNote.java
+++ src/main/java/string/RansomNote.java
@@ -1,26 +1,21 @@
 package string;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RansomNote {
 	
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < ransomNote.length(); i ++) {
-            char c = ransomNote.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        int[] freqMap = new int[26];
+        for(char c : ransomNote.toCharArray()) {
+            freqMap[c - 'a'] ++;
         }
-        for(int j = 0; j < magazine.length(); j ++) {
-            char d = magazine.charAt(j);
-            if(map.containsKey(d)) {
-                int count = map.get(d);
-                count --;
-                if(count == 0) map.remove(d);
-                else map.put(d, count);
-            }
+
+        for(char d : magazine.toCharArray()) {
+            freqMap[d - 'a'] --;
         }
-        return map.size() == 0;
+
+        for(int i = 0; i < freqMap.length; i ++) {
+            if(freqMap[i] > 0) return false;
+        }
+        return true;
     }
 
 }
