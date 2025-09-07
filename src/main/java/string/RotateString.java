7f6c09a359ed6d839c59d4ac533d924bdd995001
Vysakh-vn <vysakhvn28@gmail.com>
2025-09-07 20:39:56 +0530

    Solution of Rotate String

---
+++ src/main/java/string/RotateString.java
@@ -0,0 +1,11 @@
+package string;
+
+public class RotateString {
+	
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        String temp = s + s;
+        return temp.contains(goal);
+    }
+
+}
