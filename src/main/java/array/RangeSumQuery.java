1044e800344cda5f3d8b34b4e806679b0f1539f5
Vysakh-vn <vysakhvn28@gmail.com>
2025-09-13 09:02:06 +0530

    Solution of Range Sum Query

---
+++ src/main/java/array/RangeSumQuery.java
@@ -0,0 +1,20 @@
+package array;
+
+public class RangeSumQuery {
+	
+    int[] prefixSum;
+
+    public RangeSumQuery(int[] nums) {
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        for(int i = 1; i < nums.length; i ++) {
+            prefixSum[i] = prefixSum[i -1] + nums[i]; 
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left == 0) return prefixSum[right];
+        return prefixSum[right] - prefixSum[left - 1];
+    }
+
+}
